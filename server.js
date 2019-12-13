9acd0e8830597ed7b985c5f0aa0a94c8b9effc68
Brian Graf <brian.g.graf@gmail.com>
2019-12-12 17:43:23 -0700

    Working online/offline w/ service worker

--- server.js
+++ server.js
@@ -7,26 +7,22 @@ const app = express();
 
 app.use(logger("dev"));
 
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
 const databaseUrl = process.env.MONGODB_URI || "budget";
-const collections = ["transactions"];
+const collections = ["budget"];
 
 const db = mongojs(databaseUrl, collections);
 
 db.on("error", error => {
-  console.log("Database Error: ", error);
+  console.log("Database Error:", error);
 });
 
-app.get("/", (req, res) => {
-  res.sendStatus(200);
-});
-
-const PORT = process.env.PORT || 3600;
+const PORT = process.env.PORT || 3550;
 
 app.listen(PORT, () => {
-  console.log(`Application running on http://localhost:${PORT}`);
-})
+  console.log(`Application running on PORT ${PORT}`);
+});
